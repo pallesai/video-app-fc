ce15eafceda15e1d0df9856e33af3cb31afeb3d7
sai <pallesp@amazon.com>
2020-10-28 15:31:10 -0700

    Clean Up and add Search component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from "./Search";
+
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div className="ui container">
+                <Search />
+            </div>
+        );
+    }
+}
+
+export default App;
